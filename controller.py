b3e9b745fcb355e1b403651c862236bc5baa10f3
Eirfat Choudhury <eirfathchoudhury@gmail.com>
2023-01-03 15:09:06 +0000

    Creating a tkinter screen w/ comments

--- controller.py
+++ controller.py
@@ -3,12 +3,12 @@ from view import View
 
 class Controller():
     def __init__(self):
-        self.model = Model()
-        self.view = View(self)
+        self.model = Model() # Connecting model to controller
+        self.view = View(self) # Connecting view to controller and allowing view to connect controller to itself
     
     def main(self):
-        self.view.main()
+        self.view.main() # Running main function of view within the controller's main function
 
 if __name__ == "__main__":
-    app = Controller()
-    app.main()
+    app = Controller() # Creating a controller object
+    app.main() # Running main function of controller
--- view.py
+++ view.py
@@ -3,8 +3,8 @@ from tkinter import ttk
 
 class View(tk.Tk):
     def __init__(self, controller):
-        super().__init__()
-        self.controller = controller
+        super().__init__() # Calling the constructor method of the parent class - self=tk.Tk()
+        self.controller = controller # Connecting controller to view
     
     def main(self):
-        self.mainloop()
+        self.mainloop() # Creation of window screen
